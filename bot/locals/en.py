068ecc2e9cbd2d5955882afdf9151ab7dfb6191f
ohyeahiamunnamed <66197039+ohyeahiamunnamed@users.noreply.github.com>
2020-12-12 17:47:55 +0530

    Yeah done

--- bot/locals/en.py
+++ bot/locals/en.py
@@ -42,6 +42,6 @@ LOCAL = Local({
     'COMMAND_UPLOAD_AS_DOC' : 'toggle upload anything as document',
     'COMMAND_UPLOAD_AS_ZIP' : 'toggle upload anything as bundled zipfile',
     'COMMAND_SET_TRACKER' : 'set default tracker, sparated by newline',
-    'BLOCK_EMPTY' : "▱",
-    "BLOCK_FILLED" : "▰"
+    'BLOCK_EMPTY' : "▫",
+    "BLOCK_FILLED" : "▪"
 })
